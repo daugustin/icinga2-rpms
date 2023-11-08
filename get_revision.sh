1523b5de786748611fb8ac6852e1ce6b9f775a1d
Daniel Augustin <hossie@hossie.de>
2023-11-08 16:17:19 +0100

    First tries

--- get_revision.sh
+++ get_revision.sh
@@ -2,7 +2,7 @@
 
 ICINGA_VER=$(./get_version.sh)
 if [ $(git tag | grep -c "$ICINGA_VER") -gt 0 ]; then
-  VER=$(git tag | grep "$ICINGA_VER" | sed 's/^v//g' | tail -n 1 | sed "s#${ICINGA_VER}-##g")
+  VER=$(git tag | grep "$ICINGA_VER" | sed 's/^v*//g' | sort | uniq | tail -n 1 | sed "s#${ICINGA_VER}-##g")
   echo $((VER+1))
 else
   echo 1
